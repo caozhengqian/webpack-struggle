0c1107c6c73d5efe73c3aff3c94768bd71fddd54
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-11 15:21:23 +0800

    03开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 9000
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      title: 'Development1',
     }),
   ],
   output: {
     // filename: 'main.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
 };
