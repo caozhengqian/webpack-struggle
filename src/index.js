73b658d9e5f92e51d490c6dbe69163abe0f5b95e
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-11 10:46:54 +0800

    恢复开始学习

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
 
 console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
@@ -18,19 +11,7 @@ console.log(json.owner.name); // output `Tom Preston-Werner`
 function component() {
   const element = document.createElement('div');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  //突如其来的new Image()
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  //解析xml和csv文件
-  console.log(Data);
-  console.log(Notes);
-  console.log('我解析xml和csv完成');
 
   return element;
 }
