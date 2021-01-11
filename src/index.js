b410e2bcbce07876701b7947dba46ed1de59cddb
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-11 11:19:16 +0800

    管理输出结束

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
